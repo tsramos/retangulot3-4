94fedc3b39f28a2466210e83986d7add19acd0c0
Thiago Ramos <thiago.s.ramos@outlook.com>
2024-03-14 20:42:47 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
